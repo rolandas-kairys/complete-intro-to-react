5bddb655b964ace853197be1e54aadcaee9b3106
rolandas-kairys <rolandas.kairys@gmail.com>
2017-12-27 21:55:14 +0000

    Managing State - making a class component out of pure function

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => {
-  return (
-    <div className="search">
-      <header>
-        <h1>s-video</h1>
-        <input type="text" placeholder="Search" />
-      </header>
-      <div>
-        {preload.shows.map((show) =>
-          <ShowCard key={show.imdbID} show={show} />
-        )}
+class Search extends Component {
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>s-video</h1>
+          <input type="text" placeholder="Search" />
+        </header>
+        <div>
+          {preload.shows.map((show) =>
+            <ShowCard key={show.imdbID} show={show} />
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Search;
