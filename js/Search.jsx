de2a8ad79094fdd82987eb424155c6e499f9497e
rolandas-kairys <rolandas.kairys@gmail.com>
2017-12-28 11:56:39 +0000

    Fiultering - universal algorithm to filter content on the page

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,7 +4,7 @@ import ShowCard from './ShowCard';
 
 class Search extends Component {
   state = {
-    searchTerm: "this is some debug statement"
+    searchTerm: ""
   }
 
 
@@ -20,9 +20,16 @@ class Search extends Component {
           <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type="text" placeholder="Search" />
         </header>
         <div>
-          {preload.shows.map((show) =>
-            <ShowCard key={show.imdbID} show={show} />
-          )}
+          {preload.shows
+            .filter(
+            show =>
+              `${show.title} ${show.description}`
+                .toUpperCase().indexOf(this.state.searchTerm
+                  .toUpperCase()) >= 0
+            )
+            .map((show) =>
+              <ShowCard key={show.imdbID} show={show} />
+            )}
         </div>
       </div>
     );
