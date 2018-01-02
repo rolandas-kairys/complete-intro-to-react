d0dbb8fcf0a9fa95d1957d93d5591fe8995aa3c3
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-02 11:48:17 +0000

    Data in React - Finishing Details Component

--- js/Details.jsx
+++ js/Details.jsx
@@ -3,10 +3,28 @@
 import React from 'react';
 
 
-const Details = (props) => (
-  <div class="details">
-    <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
-  </div>
-);
-
+const Details = (props: { show: Show }) => {
+  const { title, description, year, poster, trailer } = props.show;
+  return (
+    <div class="details">
+      <header>
+        <h1>s-video</h1>
+      </header>
+      <section>
+        <h1>{title}</h1>
+        <h2>({year})</h2>
+        <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
+        <p>{description}</p>
+      </section>
+      <div>
+        <iframe
+          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+          frameborder="0"
+          allowFullScreen
+          title={`Trailer for ${title}`}
+        />
+      </div>
+    </div>
+  );
+}
 export default Details;
