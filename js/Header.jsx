1d39da5b7c847d7e035175e492653301bd2004f3
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-02 18:40:40 +0000

    Data in React - Conditional Display Logic (p1)

--- js/Header.jsx
+++ js/Header.jsx
@@ -4,14 +4,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <h1>
-      <Link to="/">
-        s-video
+const Header = (props: { showSearch?: boolean }) => {
+  let utilSpace;
+  if (props.showSearch) {
+    utilSpace = <h1>utilSpace</h1>;
+  } else {
+    utilSpace = (
+      <h2>
+        <Link to="/search">
+          Back
+        </Link>
+      </h2>
+    );
+  }
+
+
+  return (
+    <header>
+      <h1>
+        <Link to="/">
+          s-video
       </Link>
-    </h1>
-  </header>
-);
+      </h1>
+      {utilSpace}
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  showSearch: false
+};
+
 
 export default Header;
