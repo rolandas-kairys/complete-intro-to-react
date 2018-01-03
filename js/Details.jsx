9465b4235e987917ba266d5867cc71ab9eb98203
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-03 13:13:28 +0000

    Data in React - AJAX Requests: using response data inside component

--- js/Details.jsx
+++ js/Details.jsx
@@ -8,12 +8,12 @@ import Spinner from './Spinner';
 
 class Details extends Component {
   state = {
-    apiData: { imdbRating: "" }
+    apiData: { rating: "" }
   };
 
   componentDidMount() {
     axios.get(`http://localhost:3000/${this.props.show.imdbID}`)
-      .then((response: { data: { imdbRating: string } }) => {
+      .then((response: { data: { rating: string } }) => {
         this.setState({ apiData: response.data })
       });
   }
@@ -23,12 +23,21 @@ class Details extends Component {
 
   render() {
     const { title, description, year, poster, trailer } = this.props.show;
+
+    let ratingComponent;
+    if (this.state.apiData.rating) {
+      ratingComponent = <h3>{this.state.apiData.rating}</h3>;
+    } else {
+      ratingComponent = <Spinner />;
+    }
+
     return (
       <div className="details" >
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {ratingComponent}
           <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
           <p>{description}</p>
         </section>
