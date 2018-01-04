7b6a44e75af36214f406af708886d7048c4c875b
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-04 12:28:45 +0000

    Redux - Connect Redux to React

--- js/App.jsx
+++ js/App.jsx
@@ -4,6 +4,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
@@ -14,20 +16,22 @@ const FourOhFour = () => <h1>404</h1>
 
 const App = () => (
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={(props) => <Search shows={preload.shows} {...props} />} />
-        <Route path="/details/:id"
-          component={(props: { match: Match }) => {
-            console.log(props);
-            const selectedShow = preload.shows.find((show) => props.match.params.id === show.imdbID);
-            return <Details show={selectedShow} { ...props } />
-          }} />
+    <Provider store={store}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/search" component={(props) => <Search shows={preload.shows} {...props} />} />
+          <Route path="/details/:id"
+            component={(props: { match: Match }) => {
+              console.log(props);
+              const selectedShow = preload.shows.find((show) => props.match.params.id === show.imdbID);
+              return <Details show={selectedShow} { ...props } />
+            }} />
 
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+    </Provider>
   </BrowserRouter>
 );
 
