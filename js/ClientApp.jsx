53a5149a46585e14c6465247b4d49ffc0c591d39
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-29 13:16:59 +0200

    first Route created for component

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom'
 import Landing from './Landing';
 
 const App = () => (
-  <div className="app">
-    <Landing />
-  </div>
+  <HashRouter>
+    <div className="app">
+      <Route exact path="/" component={Landing} />
+    </div>
+  </HashRouter>
 );
 
 render(<App />, document.getElementById('app'));
