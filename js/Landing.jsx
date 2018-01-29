3d88c9b7b5582b580d57d6395aa2f03db06d151c
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-29 13:10:47 +0200

    new component created

---
+++ js/Landing.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  state = {}
+  render() {
+    return (
+      <div className="landing">
+        <h1>svideo</h1>
+        <input type="text" placeholder="Search" />
+        <a>or Browse All</a>
+      </div>
+    );
+  }
+}
+
+export default Landing;
